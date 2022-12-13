46ed2ac66015e5602d323d10680cab59b4a8b5b1
Lorenco Waleka <lorenco.waleka96@gmail.com>
2022-12-13 17:53:21 +0100

    5.3

---
+++ src/main/java/com/kodilla/collections/adv/immutable/homework/TaskHacked.java
@@ -0,0 +1,10 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public class TaskHacked extends Task{
+    public TaskHacked(String title, int duration){
+        super(title,duration);
+    }
+    public void modifyTitle(String newTitle){
+        title = newTitle;
+    }
+}

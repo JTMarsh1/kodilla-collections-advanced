cd4c69c91382499cc44a1657c508da470bb8064f
Lorenco Waleka <lorenco.waleka96@gmail.com>
2022-12-26 17:49:45 +0100

    5.4

--- src/main/java/com/kodilla/collections/adv/exercises/homework/Flight.java
+++ src/main/java/com/kodilla/collections/adv/exercises/homework/Flight.java
@@ -22,28 +22,8 @@ public class Flight {
     public String addFlightTo(String arrival) {
         return arrival;
     }
-    public String addFlightFrom(String departure){
+
+    public String addFlightFrom(String departure) {
         return departure;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Flight flight = (Flight) o;
-        return Objects.equals(departure, flight.departure) && Objects.equals(arrival, flight.arrival);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(departure, arrival);
-    }
-
-    @Override
-    public String toString() {
-        return "Flight{" +
-                "departure='" + departure + '\'' +
-                ", arrival='" + arrival + '\'' +
-                '}';
     }
 }

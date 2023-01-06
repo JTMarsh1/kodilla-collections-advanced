7bbf906d5418dfcb79baa924fa4f2d0b4a250d82
Lorenco Waleka <lorenco.waleka96@gmail.com>
2023-01-06 15:30:50 +0100

    6.4

--- kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseAppTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseAppTestSuite.java
@@ -7,21 +7,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class WarehouseTestSuite {
 
     @Test
-
     public void testGetOrder_withException() {
         Warehouse warehouse = new Warehouse();
-
         assertThrows(OrderDoesntExistException.class, () -> warehouse.getOrder("900"));
-
     }
 
     @Test
     public void TestGetOrder() throws OrderDoesntExistException {
         Warehouse warehouse = new Warehouse();
         warehouse.addOrder(new Order("1234"));
-
         Order expected = warehouse.getOrder("1234");
-
         Order expected2 = warehouse.getOrder("1234");
         assertNotNull(expected);
     }
